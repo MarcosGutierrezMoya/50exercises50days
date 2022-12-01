275235088a4ea34e8b6d87118671caf1da7654f7
MarcosGutierrezMoya <marcos.gutierrez.moya2010@gmail.com>
2022-12-01 09:55:47 +0100

    sound board with stopping audios

--- soundBoard/main.js
+++ soundBoard/main.js
@@ -2,7 +2,17 @@ let audios = document.getElementsByTagName("audio");
 
 for (let i = 0; i < audios.length; i++) {
     let btnAudio = document.getElementById("buttons").appendChild(document.createElement("button"))
-    btnAudio.innerText = audios[i].id.toString();
+    btnAudio.innerText = audios[i].id;
     btnAudio.classList = ("btn");
-    btnAudio.addEventListener("click",()=>audios[i].play())
+    btnAudio.addEventListener("click",()=>{ 
+        for (let e = 0; e < audios.length; e++) {
+            if (audios[i].id == audios[e].id) {
+                if(audios[i].currentTime == 0){audios[i].play()}
+                else{audios[i].pause();
+                audios[i].currentTime = 0;}
+            }
+            else{audios[e].pause();
+                audios[e].currentTime = 0;}
+        }
+    })
 }
