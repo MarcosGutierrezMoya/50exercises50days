176c5d8184809949afe7ecf817ba5272e8d352a8
MarcosGutierrezMoya <marcos.gutierrez.moya2010@gmail.com>
2022-12-13 17:58:45 +0100

    faq-collapse first commit

--- event_keycodes/main.js
+++ event_keycodes/main.js
@@ -7,30 +7,29 @@ document.addEventListener('keydown',(function(event){
     for (let i = 0; i < 3; i++) {
         const divParent = insert.appendChild(document.createElement("div"));
         divParent.classList.add("parent"); 
+        const text = divParent.appendChild(this.createElement("p"));
+        const key = divParent.appendChild(document.createElement("div"));
         switch (i) {
             case 0:
-                const keySection = divParent.appendChild(this.createElement("p"));
-                keySection.innerText = "event.key";
-                keySection.classList.add("text");
-                const key = divParent.appendChild(document.createElement("div"));
+                text.innerText = "event.key";
+                text.classList.add("text");
+
                 key.classList.add("box");
                 key.innerText = event.key;
                 break;
             case 1:
-                const keyCodeSection = divParent.appendChild(this.createElement("p"));
-                keyCodeSection.innerText = "event.keyCode";
-                keyCodeSection.classList.add("text");
-                const keyCode = divParent.appendChild(document.createElement("div"));
-                keyCode.classList.add("box");
-                keyCode.innerText = event.key.codePointAt();
+                text.innerText = "event.keyCode";
+                text.classList.add("text");
+
+                key.classList.add("box");
+                key.innerText = event.which;
                 break;
             case 2:
-                const codeSection = divParent.appendChild(this.createElement("p"));
-                codeSection.innerText = "event.code";
-                codeSection.classList.add("text");;
-                const code = divParent.appendChild(document.createElement("div"));
-                code.classList.add("box");
-                code.innerText = event.code;                
+                text.innerText = "event.code";
+                text.classList.add("text");
+                
+                key.classList.add("box");
+                key.innerText = event.code;                
                 break;
         }        
     }
---
+++ faq_collapse/main.js
@@ -0,0 +1,5 @@
+const x = document.getElementsByClassName("fa-times");
+
+for (let i = 0; i < x.length; i++) {
+    x[i].classList.remove("fa-times");
+}
