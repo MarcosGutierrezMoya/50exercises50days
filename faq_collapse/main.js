47f1de616e74e83ac3f640d2effac0446eaa0a3b
MarcosGutierrezMoya <marcos.gutierrez.moya2010@gmail.com>
2022-12-13 20:22:13 +0100

    faq collapse exercise

--- faq_collapse/main.js
+++ faq_collapse/main.js
@@ -1,5 +1,20 @@
-const x = document.getElementsByClassName("fa-times");
+let openBtn = document.getElementsByClassName("fa-chevron-down");
+console.log(openBtn);
 
-for (let i = 0; i < x.length; i++) {
-    x[i].classList.remove("fa-times");
+for (let i = 0; i < openBtn.length; i++) {
+    openBtn[i].addEventListener('click',(event)=>{
+        if (event.target.classList.contains("fa-times")) {
+            event.target.parentElement.previousSibling.previousSibling.classList.remove("activeText");
+            event.target.parentElement.parentElement.classList.remove("active");
+            event.target.classList.remove("fa-times");
+            event.target.classList.add("fa-chevron-down");
+        }
+        else{
+            event.target.parentElement.previousSibling.previousSibling.classList.add("activeText");
+            event.target.parentElement.parentElement.classList.add("active");
+            event.target.classList.add("fa-times");
+            event.target.classList.remove("fa-chevron-down");
+        }
+        
+    });
 }
