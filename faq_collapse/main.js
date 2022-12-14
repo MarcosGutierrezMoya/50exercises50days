3f2338564120bab09bd82530af6a53e11a1a3962
MarcosGutierrezMoya <marcos.gutierrez.moya2010@gmail.com>
2022-12-14 12:35:29 +0100

    Random choice picker exercise

--- faq_collapse/main.js
+++ faq_collapse/main.js
@@ -4,17 +4,16 @@ console.log(openBtn);
 for (let i = 0; i < openBtn.length; i++) {
     openBtn[i].addEventListener('click',(event)=>{
         if (event.target.classList.contains("fa-times")) {
-            event.target.parentElement.previousSibling.previousSibling.classList.remove("activeText");
+            event.target.parentElement.previousSibling.previousSibling.classList.remove("active");
             event.target.parentElement.parentElement.classList.remove("active");
             event.target.classList.remove("fa-times");
             event.target.classList.add("fa-chevron-down");
         }
         else{
-            event.target.parentElement.previousSibling.previousSibling.classList.add("activeText");
+            event.target.parentElement.previousSibling.previousSibling.classList.add("active");
             event.target.parentElement.parentElement.classList.add("active");
             event.target.classList.add("fa-times");
             event.target.classList.remove("fa-chevron-down");
         }
-        
     });
 }
---
+++ randomChoicePicker/script.js
@@ -0,0 +1,40 @@
+const textArea = document.getElementById("textarea");
+const tags = document.getElementById("tags");
+let text = "";
+let rdm = -1;
+let interval;
+let options;
+let rdmSelect = false;
+
+textArea.addEventListener("keydown",splitText)
+
+function splitText(event){
+    if (rdmSelect) {
+        rdmSelect = false;
+        tags.innerHTML = "";
+    }
+    if(event.target.value[event.target.value.length-1] === ","){
+        tags.innerHTML += `<p class="option">${text}</p>`;
+    }
+    else if(event.key === "Enter"){
+        event.preventDefault();
+        options = document.getElementsByClassName("option");
+        interval = setInterval(changeColor, 300);
+        event.target.value = null;
+        setTimeout(() => {
+            clearInterval(interval);
+            rdmSelect = true;
+        }, 3000);
+    }
+    else{
+        text = event.target.value.slice(event.target.value.lastIndexOf(",") + 1);
+    }
+}
+
+function changeColor() {
+    if (rdm !== -1) {
+        options[rdm].classList.remove("active")
+    }
+    rdm = Math.floor(Math.random()*(options.length));
+    options[rdm].classList.add("active");
+}
